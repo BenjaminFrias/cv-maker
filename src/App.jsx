4b609f8a6cd68ba81f6002b01ec5ee5b35f263f1
benjaminfrias <benjaminfrias2004@gmail.com>
2025-03-06 13:44:43 -0400

    add: basic form structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // import { useState } from 'react'
 import './App.css'
+import './components/InputPage'
 
 function App() {
-
   return (
-    <>
-    </>
+    <InputPage></InputPage>
   )
 }
 
 export default App
+  
---
+++ src/components/InputPage.jsx
@@ -0,0 +1,19 @@
+function InputPage() {
+    return (
+      <form>
+          <InputSection title={"General information"}>
+            <GeneralInfoInputs/>
+          </InputSection>
+  
+          <InputSection title={"Educational experience"}>
+            <EducationalInputs/>
+          </InputSection>
+          
+          <InputSection title={"Practical experience"}>
+            <PracticalInputs/>
+          </InputSection>
+  
+          <SubmitBtn/>
+      </form>
+    )
+  }
