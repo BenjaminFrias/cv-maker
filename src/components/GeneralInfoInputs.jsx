fd83342ac69faeb25e1b9abf2fda44d3acb4b618
benjaminfrias <benjaminfrias2004@gmail.com>
2025-03-07 15:16:17 -0400

    split full name into two inputs

--- src/components/GeneralInfoInputs.jsx
+++ src/components/GeneralInfoInputs.jsx
@@ -3,9 +3,10 @@ import InputField from "./InputField";
 function GeneralInfoInputs() {
 	return (
 		<div>
-			<InputField placeholder={"Full name:"} type={"text"}/>
+			<InputField placeholder={"First name:"} type={"text"}/>
+			<InputField placeholder={"Last name:"} type={"text"}/>
 			<InputField placeholder={"Email:"} type={"email"}/>
-			<InputField placeholder={"Phone:"} type={"number"}/>
+			<InputField placeholder={"Phone:"} type={"text"}/>
 		</div>
 	)
 }
