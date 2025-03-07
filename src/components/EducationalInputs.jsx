e2f6e735f719eb84667123866bba016e9d15e8fa
benjaminfrias <benjaminfrias2004@gmail.com>
2025-03-07 17:37:34 -0400

    feature: add edu and prac sections with btn

--- src/components/EducationalInputs.jsx
+++ src/components/EducationalInputs.jsx
@@ -3,9 +3,9 @@ import InputField from "./InputField";
 function EducationalInputs() {
 	return (
 			<div>
-			<InputField placeholder={"School name:"} type={"text"}/>
-			<InputField placeholder={"Title of study:"} type={"text"}/>
-			<InputField placeholder={"Date of study:"} type={"date"}/>
+				<InputField placeholder={"School name:"} type={"text"}/>
+				<InputField placeholder={"Title of study:"} type={"text"}/>
+				<InputField placeholder={"Date of study:"} type={"date"}/>
 			</div>
 	)
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,21 +4,53 @@ import EducationalInputs from "./EducationalInputs"
 import PracticalInputs from "./PracticalInputs"
 import SubmitBtn from "./SubmitBtn"
 import "../styles/Form.css"
+import { useState } from "react"
+
 
 function Form() {
-    return <form className="cv-form">
-            <InputSection title={"General information"}>
-              <GeneralInfoInputs/>
-            </InputSection>
-            <InputSection title={"Educational experience"}>
-              <EducationalInputs/>
-            </InputSection>
-        
-            <InputSection title={"Practical experience"}>
-              <PracticalInputs/>
-            </InputSection>
-            <SubmitBtn/>
-        </form>
+
+  // Educational sections states
+  const [eduSections, setEduSections] = useState([<InputSection title={"Educational information"} key={`edu-${Date.now()}-0`}>
+    <EducationalInputs/>
+  </InputSection>,]);
+
+const handleAddEduSection = () => {
+  const newKey = `edu-${Date.now()}-${eduSections.length}`;
+  setEduSections([...eduSections, <InputSection title={"Educational information"} key={newKey}>
+      <EducationalInputs/>
+    </InputSection>,]);
+  };
+  
+  // Educational sections states
+  const [pracSections, setPracSections] = useState([<InputSection title={"Practical information"} key={`prac-${Date.now()}-0`}>
+    <PracticalInputs/>
+  </InputSection>,]);
+
+const handleAddPracSection = () => {
+    const newKey = `prac-${Date.now()}-${pracSections.length}`;
+    setPracSections([...pracSections, <InputSection title={"Practical information"} key={newKey}>
+      <PracticalInputs/>
+    </InputSection>,]);
+  };
+
+
+  return <form className="cv-form">
+    <InputSection title={"General information"}>
+      <GeneralInfoInputs/>
+    </InputSection>
+
+    {eduSections.map(sec => sec)}
+    <button type="button" onClick={handleAddEduSection}>
+      Add educational Experience
+    </button>
+    
+    {pracSections.map(sec => sec)}
+    <button type="button" onClick={handleAddPracSection}>
+      Add Practical Experience
+    </button>
+
+    <SubmitBtn/>
+  </form>
   }
 
   export default Form;
