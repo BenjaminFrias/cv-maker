c5dff04dd9042405e6bd2f09c4463210f1cf2a3d
benjaminfrias <benjaminfrias2004@gmail.com>
2025-03-08 12:09:42 -0400

    clear console every HMR reload

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// clear console every HMR reload
+if (import.meta.hot) {
+  import.meta.hot.on(
+    "vite:beforeUpdate",
+    () => console.clear()
+  );
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
