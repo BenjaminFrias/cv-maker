b2162b8a783603ac775a57bff022f8aab476bc74
benjaminfrias <benjaminfrias2004@gmail.com>
2025-03-08 18:37:45 -0400

    style cv result page

--- src/components/CvResult.jsx
+++ src/components/CvResult.jsx
@@ -1,10 +1,12 @@
+import "../styles/CvResult.css"
+
 function CvResult() {
     return <>
         <div className="cv-result">
-            <h2>Carles Puyol Saforcada</h2>
-            <hr/>
+            <h2 className="name">Carles Puyol Saforcada</h2>
             <div className="general-info-container">
                 <p className="email">contact@carlespuyol.com</p>
+                <p>|</p>
                 <p className="phone">917572XXXX</p>
             </div>
             <hr/>
@@ -12,8 +14,8 @@ function CvResult() {
             <div className="section-container">
                 <h2>Educational experience</h2>
 
-                <div className="section">
-                    <p className="school-name">La Masia</p>
+                <div className="section educational-section">
+                    <h3 className="school-name">La Masia</h3>
                     <p className="title-of-study">Canterano</p>
                     <p className="date">2 de octubre de 1999</p>
                 </div>
@@ -23,15 +25,15 @@ function CvResult() {
             <div className="section-container">
                 <h2>Practical experience</h2>
 
-                <div className="section">
-                    <p className="company-name">Fútbol Club Barcelona "B"</p>
+                <div className="section practical-section">
+                    <h3 className="company-name">Fútbol Club Barcelona "B"</h3>
                     <p className="position-title">Lateral derecho</p>
                     <p className="main-responsabilities">Defender y atacar</p>
                     <p className="date">13/04/1995 - 23/10/1996</p>
                 </div>
 
-                <div className="section">
-                    <p className="company-name">Barcelona F.C</p>
+                <div className="section practical-section">
+                    <h3 className="company-name">Barcelona F.C</h3>
                     <p className="position-title">Defensor central</p>
                     <p className="main-responsabilities">Defender y capitan</p>
                     <p className="date">02/10/1999 - 04/03/2014</p>
