b729ee2f2c08d91d4c321a120280a507c37661da
benjaminfrias <benjaminfrias2004@gmail.com>
2025-03-09 15:44:17 -0400

    add required attribute in inputs

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,7 +1,7 @@
 import "../styles/InputField.css"
 
 function InputField({type, placeholder}) {
-  return <input type={type} placeholder={placeholder} className="input-field"></input>
+  return <input type={type} placeholder={placeholder} className="input-field" required="true"></input>
 }
 
 export default InputField;
